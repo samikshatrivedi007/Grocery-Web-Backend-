eb6a2917034a9856b5a9146ff025451652453ac0
Samiksha <samikshatrivedi007@gmail.com>
2025-07-08 01:07:54 +0530

    sever connected successfully!!

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+console.log("🟡 Starting server...");
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 5000;
 app_1.default.listen(PORT, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+console.log("🟡 Starting server...");
+
 import app from "./app";
 
 const PORT = process.env.PORT || 5000;
